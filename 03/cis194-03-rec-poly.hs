199e98ced368fed35aa167d0e38dee6c46d1f8b7
Jussi Hallila <jussi@hallila.com>
2015-05-19 16:14:40 +0100

    Update cis194-03-rec-poly.hs

--- 03/cis194-03-rec-poly.hs
+++ 03/cis194-03-rec-poly.hs
@@ -1,6 +1,6 @@
 module Golf where
 
--- Exercise 1
+-- Exercise 1 - Hopscotch
 skips :: [a] -> [[a]]
 skips [] = []
 skips xs = map (nth xs) [1..(length xs)]
@@ -9,3 +9,10 @@ nth :: [a] -> Int -> [a]
 nth xs 1 = xs
 nth [] _ = []
 nth xs n = take 1 (drop (n-1) xs) ++ (nth (drop n xs) (n))
+
+-- Exercise 2 Local Maxima
+localMaxima :: [Integer] -> [Integer]
+localMaxima (x:y:z:xs) 
+ | y > x && y > z = y : (localMaxima (y:(z:xs)))
+ | otherwise = localMaxima (y:(z:xs))
+localMaxima _ = []
