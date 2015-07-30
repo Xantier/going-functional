4458382dd44e043c68e3af9d3f90d9d99ed9bc68
Jussi Hallila <jussi@hallila.com>
2015-07-30 10:00:15 -0400

    Update cis194-05-type-classes.hs

--- 05/cis194-05-type-classes.hs
+++ 05/cis194-05-type-classes.hs
@@ -36,3 +36,15 @@ instance Expr Bool where
   lit = (>0)
   add = (||)
   mul = (&&)
+  
+newtype MinMax = MinMax Integer deriving (Eq, Show)
+instance Expr MinMax where
+  lit = MinMax
+  add (MinMax x) (MinMax y)= MinMax (max x y)
+  mul (MinMax x) (MinMax y)= MinMax (min x y)
+  
+newtype Mod7 = Mod7 Integer deriving (Eq, Show)
+instance Expr Mod7 where
+  lit = Mod7 . (`mod` 7)
+  add (Mod7 x) (Mod7 y) = Mod7 $ (x + y) `mod` 7
+  mul (Mod7 x) (Mod7 y) = Mod7 $ (x * y) `mod` 7
