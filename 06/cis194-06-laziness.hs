5db9f8623b3c3aee719903078edbaba69ed34a43
Jussi Hallila <jussi@hallila.com>
2015-07-31 07:30:05 -0400

    Create cis194-06-laziness.hs

---
+++ 06/cis194-06-laziness.hs
@@ -0,0 +1,35 @@
+-- Exercise 1
+
+fib :: Integer -> Integer
+fib 0 = 0
+fib 1 = 1
+fib n = fib (n-1) + fib (n-2)
+
+fibs1 :: [Integer]
+fibs1 = map fib [0..]
+
+-- Exercise 2 :D
+fibs2 :: [Integer]
+fibs2 = 0:1:zipWith (+) fibs3 (tail fibs3)
+
+fibo :: Integer -> Integer -> [Integer]
+fibo a b = a : fibo b (a+b)
+
+fibs3 :: [Integer]
+fibs3 = fibo 0 1
+
+-- Exercise 3
+data Stream a = Cons a (Stream a)
+
+instance Show a => Show (Stream a) where
+  show = show . take 20 . streamToList
+
+streamToList :: Stream a -> [a]
+streamToList (Cons x xs) = x : streamToList xs
+
+-- Exercise 4
+streamRepeat :: a -> Stream a
+streamRepeat x = Cons x (streamRepeat x)
+
+streamMap :: (a -> b) -> Stream a -> Stream b
+streamMap f (Cons x xs) = Cons (f x) (streamMap f xs)
